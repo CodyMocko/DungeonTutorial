6304f24f919eb38362066a02d9ecc3ec132a556d
Cody Mocko <cody.mocko@yahoo.com>
2022-09-17 22:46:35 -0700

    Added a box component for overlap events

---
+++ Source/DungeonTutorial/TriggerComponent.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when 
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Component works!"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Warning, TEXT("Component works! Tick tick"));
+}
